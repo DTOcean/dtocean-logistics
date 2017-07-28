4af4d5670b3e1e9987aea953c4b73e2b6a74cfc9
Mathew Topper <damm_horse@yahoo.co.uk>
2017-07-28 15:05:10 +0100

    Update version number to 1.1.dev1 and add lintly configuration file.

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ class CleanPyc(Command):
                 yield os.path.join(root, fname)
 
 setup(name='dtocean-logistics',
-      version='1.1.dev0',
+      version='1.1.dev1',
       description='Logistics module for the DTOcean tools',
       author=('Paulo Chainho, '
               'Boris Teillant, '
