147f18172a2784994a3d79731695ddd4752682e7
Mathew Topper <damm_horse@yahoo.co.uk>
2017-07-28 10:08:00 +0100

    Reduce depths of imports for scheduling functions

---
+++ dtocean_logistics/performance/schedule/install/__init__.py
@@ -0,0 +1,15 @@
+
+from .install_dev.schedule_dev import sched_dev
+from .install_elec.schedule_e_export import sched_e_export
+from .install_elec.schedule_e_array import sched_e_array
+from .install_elec.schedule_e_dynamic import sched_e_dynamic
+from .install_elec.schedule_e_cp_seabed import sched_e_cp_seabed
+from .install_elec.schedule_e_cp_surface import sched_e_cp_surface
+from .install_elec.schedule_e_external import sched_e_external
+from .install_mf.schedule_driven import sched_driven
+from .install_mf.schedule_gravity import sched_gravity
+from .install_mf.schedule_m_drag import sched_m_drag
+from .install_mf.schedule_m_direct import sched_m_direct
+from .install_mf.schedule_m_suction import sched_m_suction
+from .install_mf.schedule_m_pile import sched_m_pile
+from .install_mf.schedule_s_struct import sched_s_struct
